15ac1861473780fddd5002b09248cb34f1f26d8b
Vinod <vinod@dreamguys.co.in>
2019-06-26 17:44:27 +0530

    home page changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="My First React Website" />
+    <h1>React Website</h1>
+    <p>React content goes here...</p>
+    <p>React content goes here...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
